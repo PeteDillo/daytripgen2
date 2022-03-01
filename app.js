e9642c15a9d472f0469009c348b9f3e357489a76
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-03-01 15:25:55 -0500

    fixed while loop to keep prompting user until they are happy with their trip fixed some grammer issues

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 //fill in the arrays with things
-const locations =[];
-const restaurants =[];
-const entertainment =[];
-const transportation =[];
-
+const locations =['Mexico', 'South Korea', 'Poland', 'Canada', 'New Jersey'];
+const restaurants =['mexican', 'chinese', 'italian', 'french', 'greek'];
+const entertainment =['movie','stand-up','concert','musical','bowling'];
+const transportation =['train','bus','car','boat','helicopter'];
 //set these equal to variables
 let pickedLocation = RandomItemPicker(locations)
 let pickedRestaurant = RandomItemPicker(restaurants)
 let pickedEntertainment = RandomItemPicker(entertainment)
 let pickedTransportation = RandomItemPicker(transportation)
-
 //function for random item from an array
 function RandomItemPicker(array){
-    return[array[Math.floor(math.random()*array.length)]]
+    return array[Math.floor(Math.random()*array.length)]
 }
 // change variables name
-function Reselector (array){
+function Reselector (choice){
 switch(choice){
-    case 'location':
+    case '1': //location
         return pickedLocation = RandomItemPicker(locations)
-
-    case 'restaurant':
+    case '2': //restaurant
         return pickedRestaurant = RandomItemPicker(restaurants)    
-
-    case 'entertainment':
+    case '3': //entertainment
         return  pickedEntertainment = RandomItemPicker(entertainment)   
-
-    case 'transportation':
-        return pickedTransportation = RandomItemPicker(transportation)   
-
+    case '4': //transportation
+        return pickedTransportation = RandomItemPicker(transportation)
+    case '0': //reset whole trip
+        pickedLocation = RandomItemPicker(locations)
+        pickedRestaurant = RandomItemPicker(restaurants)
+        pickedEntertainment = RandomItemPicker(entertainment) 
+        pickedTransportation = RandomItemPicker(transportation)
+        break;  
     default:
         alert('not a valid answer please try again')
         Reselector (array)
 }
 };
-
-//write the actual message function that works as the main function.
-
+//write the actual message showing.
 let userHappy = false
-
 do{
+    userHappy = confirm(`Hello, please confirm your trip to ${pickedLocation}, travel via ${pickedTransportation}, fine dinning at the local ${pickedRestaurant} restaurant and enjoy a ${pickedEntertainment}.`);
     if(userHappy === false){
-
-    }
-    else{
-        
+        let reselectItem = prompt(`Enter "1" to change location ${pickedLocation}. Enter "2" to change dining at a ${pickedRestaurant} restaurant. Enter "3" to change entertainment ${pickedEntertainment} Enter "4" to change transpotation type ${pickedTransportation} Enter "0" to changed entire trip.`)
+        Reselector(reselectItem)
     }
 }
 while(userHappy === false)
+alert('Trip completed')
